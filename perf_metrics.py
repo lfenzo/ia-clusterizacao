5ae01c5fc1407f5b68f81ea8e8615cfdc5acf8b2
Enzo <lfenzo@protonmail.com>
2021-05-17 21:05:15 -0300

    Melhoria no Perf. Metrics

--- perf_metrics.py
+++ perf_metrics.py
@@ -17,8 +17,6 @@ from sklearn.metrics import adjusted_rand_score
 if __name__ == '__main__':
 
     reference_cluster_files = [file for file in os.listdir('./datasets/') if '.clu' in file]
-    kmeans_pred_files       = [file for file in os.listdir('./previsoes') if 'kmeans'     in file]
-    singlelink_pred_files   = [file for file in os.listdir('./previsoes') if 'singlelink' in file]
 
     # informações usadas para construir o dataframe
     perf_info = {
@@ -33,48 +31,32 @@ if __name__ == '__main__':
 
         dataset_prefix = reference_file.split('R')[0]
 
-        """ Obtem todas as classificações feitas com o algoritmo KMeans """
-        corr_pred_files = [file for file in kmeans_pred_files if dataset_prefix in file]
+        for alg in ['kmeans', 'singlelink']:
 
-        for file in corr_pred_files:
+            # encontra todos os arquivos de previsão daquele algoritmo
+            alg_files = [file for file in os.listdir('./previsoes') if alg in file]
 
-            real = pd.read_csv(f'./datasets/{reference_file}', sep = '\t', names = ['id', 'label']).sample(frac = 0.01)
-            pred = pd.read_csv(f'./previsoes/{file}')
+            # encontra todos os arquivos correspondentes ao 'reference_file' que são do algortimo 'alg'
+            corr_pred_files = [file for file in alg_files if dataset_prefix in file]
 
-            real_array = real[['label']].values.ravel()
-            pred_array = pred[['label']].values.ravel()
+            for file in corr_pred_files:
 
-            rand = adjusted_rand_score(real_array, pred_array)
+                real = pd.read_csv(f'./datasets/{reference_file}', sep = '\t', names = ['id', 'label']).sample(frac = 0.01)
+                pred = pd.read_csv(f'./previsoes/{file}')
 
-            k_count = int(file.split(')')[0].split('(')[1])
+                real_array = real[['label']].values.ravel()
+                pred_array = pred[['label']].values.ravel()
 
-            perf_info['alg'].append('kmeans')
-            perf_info['dataset'].append(dataset_prefix)
-            perf_info['k'].append(k_count)
-            perf_info['rand'].append(rand)
+                rand = adjusted_rand_score(real_array, pred_array)
 
+                k_count = int(file.split(')')[0].split('(')[1])
 
-        """ Obtem todas as classificações feitas com o algoritmo KMeans """
-        corr_pred_files = [file for file in singlelink_pred_files if dataset_prefix in file]
-
-        for file in corr_pred_files:
-
-            real = pd.read_csv(f'./datasets/{reference_file}', sep = '\t', names = ['id', 'label']).sample(frac = 0.01)
-            pred = pd.read_csv(f'./previsoes/{file}')
-
-            real_array = real[['label']].values.ravel()
-            pred_array = pred[['label']].values.ravel()
-
-            rand = adjusted_rand_score(pred_array, real_array)
-
-            k_count = int(file.split(')')[0].split('(')[1])
-
-            perf_info['alg'].append('singlelink')
-            perf_info['dataset'].append(dataset_prefix)
-            perf_info['k'].append(k_count)
-            perf_info['rand'].append(rand)
-
+                perf_info['alg'].append(alg)
+                perf_info['dataset'].append(dataset_prefix)
+                perf_info['k'].append(k_count)
+                perf_info['rand'].append(rand)
 
     perf_report = pd.DataFrame().from_dict(perf_info)
     perf_report.to_csv('performance_report.csv', index = False)
     print(perf_report)
+
